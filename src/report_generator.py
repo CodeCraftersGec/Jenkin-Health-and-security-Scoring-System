0900b7fff454762c55b6333906e15fa5f017eed3
shaktibiplabDev <shaktibiplab@gmail.com>
2025-11-05 00:53:35 +0530

    fix

--- src/report_generator.py
+++ src/report_generator.py
@@ -17,7 +17,7 @@ def generate_html_report():
     <body>
         <h1>Dogecoin Price Prediction Report</h1>
         <pre>{metrics}</pre>
-        <img src="data:image/png;base64,{encoded}" width="600">
+        <img src='../artifact/reports/doge_plot.png' width='600'>
     </body>
     </html>
     """
